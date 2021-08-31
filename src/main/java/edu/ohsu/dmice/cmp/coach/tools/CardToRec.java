3ffa5411a3b815e6c663360b6d80d6f1358eec28
Matthew Storer <storer@ohsu.edu>
2021-08-31 16:55:48 -0700

    updated zero-parameter documentation to reflect Maven execution as opposed to Jar execution

--- src/main/java/edu/ohsu/dmice/cmp/coach/tools/CardToRec.java
+++ src/main/java/edu/ohsu/dmice/cmp/coach/tools/CardToRec.java
@@ -3,9 +3,9 @@ package edu.ohsu.dmice.cmp.coach.tools;
 public class CardToRec {
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.out.println("usage: java -jar CardToRec.jar <XLSX file> <recommendation #>");
-            System.out.println("       <XLSX file> : HBP CDS Cards v5 or greater Excel spreadsheet");
-            System.out.println("       <recommendation #> : a value from Column A (Recommendation #) in the spreadsheet");
+            System.out.println("usage: mvn exec:java \"-Dexec.args=<file> <recNo>\"");
+            System.out.println("       <file>  : HBP CDS Cards v5 or greater Excel spreadsheet");
+            System.out.println("       <recNo> : a value from Column A (Recommendation #) in the spreadsheet");
             System.exit(0);
         }
 
